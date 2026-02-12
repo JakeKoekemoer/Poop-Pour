abfe4f29b04794283c7a7cf095cb1112feb93983
Jacques Koekemoer <JakeKoekemoer@gmail.com>
2026-02-12 08:29:40 +0200

    Aspire Orchestration

--- Orchestration/PoopNPour.AppHost/AppHost.cs
+++ Orchestration/PoopNPour.AppHost/AppHost.cs
@@ -1,3 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Add the API project
+var api = builder.AddProject<Projects.PoopNPour_Api>("api")
+    .WithExternalHttpEndpoints();
+
+// Add the WebApp (Vue.js/Vite) project
+var webapp = builder.AddJavaScriptApp("webapp", "../../src/PoopNPour.WebApp", "dev")
+    .WithWorkingDirectory("../../src/PoopNPour.WebApp")
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .WithEnvironment("VITE_API_URL", api.GetEndpoint("http"))
+    .WithReference(api)
+    .WaitFor(api);
+
 builder.Build().Run();
