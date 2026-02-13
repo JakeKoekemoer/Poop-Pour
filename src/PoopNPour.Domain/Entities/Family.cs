ce6633af2ba0d42333501620bbb39b4fc06dc934
Jacques Koekemoer <JakeKoekemoer@gmail.com>
2026-02-13 20:01:22 +0200

    Family Data Entity

---
+++ src/PoopNPour.Domain/Entities/Family.cs
@@ -0,0 +1,13 @@
+﻿using PoopNPour.Domain.Common;
+
+namespace PoopNPour.Domain.Entities;
+
+public class Family : BaseAuditableEntity
+{
+    // PK
+    public Guid FamilyId { get; set; }
+
+    // Properties
+    public string FamilyName { get; set; } // User friendly name for the family
+    public string FamilyLastName { get; set; } // The last name of the family, used for sorting and display purposes
+}
---
+++ src/PoopNPour.Infrastructure/Migrations/20260213175914_FamilyTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PoopNPour.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FamilyTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Families",
+                columns: table => new
+                {
+                    FamilyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FamilyName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    FamilyLastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CreatedOn = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModifiedOn = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Families", x => x.FamilyId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Families");
+        }
+    }
+}
